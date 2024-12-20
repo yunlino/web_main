da648ef468b83e787ed37af7cea69e2c407ab22e
yunlino <yanbinbook@163.com>
2024-12-21 00:42:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()], 
-  base: '/web_main/',
+  base: '/',
   publicDir: 'public',
   build: {
     outDir: 'docs'
